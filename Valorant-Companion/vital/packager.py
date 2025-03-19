c8f4465391e4ca5024b804ae303818c311b63e6f
craftdav <ocskodav@gmail.com>
2025-03-19 20:00:57 +0100

    Packager converts pictures to string for publish

--- Valorant-Companion/vital/packager.py
+++ Valorant-Companion/vital/packager.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import base64
+
+
 
 def get_resource_path(relative_path):
     """ Visszaadja az adatfájlok helyes elérési útját PyInstaller környezetben """
@@ -11,7 +14,33 @@ def get_resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 # Példa a helyes elérési út használatára
-path = get_resource_path("Valorant-Companion/")
+path = get_resource_path("")
+ipath = os.path.join(path, "assets", "Lineups")
+encoded_path = os.path.join(path, "encoded")
+
+# Végigmegyünk az assets/Lineups teljes fájlszerkezetén
+for root, dirs, files in os.walk(ipath):
+    # Az eredeti struktúra megtartásához kinyerjük a relatív elérési utat
+    relative_root = os.path.relpath(root, ipath)
+    encoded_dir = os.path.join(encoded_path, relative_root)
+
+    # Létrehozzuk a megfelelő mappát az encoded könyvtárban
+    os.makedirs(encoded_dir, exist_ok=True)
+
+    # Végigmegyünk az összes fájlon a mappán belül
+    for file in files:
+        source_file = os.path.join(root, file)
+        encoded_file = os.path.join(encoded_dir, file + ".txt")
+
+        # Base64 kódolás és mentés
+        with open(source_file, "rb") as img, open(encoded_file, "w") as f:
+            encoded_data = base64.b64encode(img.read()).decode("utf-8")
+            f.write(encoded_data)
+            f.close()
+print("Minden fájl sikeresen átkonvertálva és mentve az encoded mappába!")
+
+
+
 
 """
 To exe
